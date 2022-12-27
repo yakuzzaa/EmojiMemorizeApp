586a18c04e112f52d9d7d254866d85c53774d409
yakuzzaa <chornaya.gryazz@gmail.com>
2022-12-27 22:31:52 +0400

    Initial Commit

---
+++ EmojiMemorize/EmojiMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiMemorizeApp.swift
+//  EmojiMemorize
+//
+//  Created by polina on 27.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
